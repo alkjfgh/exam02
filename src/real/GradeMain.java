c8edd65748306404ea8c4306c16cd64326ab9865
alkjfgh <alkjfgh@naver.com>
2020-08-27 16:28:17 +0900

    20200826

--- src/real/GradeMain.java
+++ src/real/GradeMain.java
@@ -1,25 +1,32 @@
 package real;
 
+import java.util.Scanner;
+
 public class GradeMain {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		GradeDataVO vo = new GradeDataVO();
-		DataInput inputData = new DataInput();
+		System.out.print("인원수를 입력하세요 : ");
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.next());
+		for (int i = 0; i < n; i++) {
+			GradeDataVO vo = new GradeDataVO();
+			DataInput inputData = new DataInput();
+			vo.setStudentName(inputData.strDataInput("이름"));
+			vo.setStudentId(inputData.intDataInput("학번"));
+			vo.setKor(inputData.intDataInput("국어 점수"));
+			vo.setEng(inputData.intDataInput("영어 점수"));
+			vo.setMat(inputData.intDataInput("수학 점수"));
+			vo.setHis(inputData.intDataInput("한국사 점수"));
+			vo.setEdps(inputData.intDataInput("전산 점수"));
 
-		vo.setStudentName(inputData.strDataInput("이름"));
-		vo.setStudentId(inputData.intDataInput("학번"));
-		vo.setKor(inputData.intDataInput("국어 점수"));
-		vo.setEng(inputData.intDataInput("영어 점수"));
-		vo.setMat(inputData.intDataInput("수학 점수"));
-		vo.setHis(inputData.intDataInput("한국사 점수"));
-		vo.setEdps(inputData.intDataInput("전산 점수"));
+			GradeProcess gradeProcess = new GradeProcess(vo);
+			vo.setTotal(gradeProcess.total());
+			vo.setAvg(gradeProcess.avg(5));
 
-		GradeProcess gradeProcess = new GradeProcess(vo);
-		vo.setTotal(gradeProcess.total());
-		vo.setAvg(gradeProcess.avg(5));
-		
-		GradePrint gradePrint = new GradePrint();
-		gradePrint.printData(vo);
+			GradePrint gradePrint = new GradePrint();
+			gradePrint.printData(vo);
+		}
 	}
 
 }
--- src/real/GradePrint.java
+++ src/real/GradePrint.java
@@ -2,7 +2,7 @@ package real;
 
 public class GradePrint {
 	public void printData(GradeDataVO vo) {
-		System.out.println(vo.getStudentName() + " 학생\n" + vo.getStudentId() + " 학번\n" + "총점 : " + vo.getTotal() + "점\n"
-				+ "평균 : " + vo.getAvg() + "점");
+		System.out.println("이름 : "+vo.getStudentName()+"\n학번 : "+ vo.getStudentId() + "\n총점 : " + vo.getTotal() + "점"
+				+ "\n평균 : " + vo.getAvg() + "점\n\n");
 	}
 }
